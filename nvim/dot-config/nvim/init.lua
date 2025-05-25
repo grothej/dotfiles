d8eda191f3d8b4c6e741aeb9a2cf83a2423e3061
grothej <justinmaxgrothe@gmail.com>
2025-05-25 19:32:29 +0200

    Move git plugins and lua lsp plugins in seperate lua files

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -183,19 +183,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-
   { import = 'plugins' },
   { import = 'plugins.lsp' },
 
@@ -261,38 +248,6 @@ require('lazy').setup({
       filters = {
         custom = {},
       },
-    },
-  },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'sindrets/diffview.nvim',
-        lazy = false,
-        keys = {
-          { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = '[G]it diffview' },
-        },
-      },
-      'nvim-telescope/telescope.nvim',
-    },
-    opts = {},
-    keys = {
-      { '<leader>gg', '<cmd>Neogit kind=floating<CR>', desc = '[G]it' },
-      { '<leader>gp', '<cmd>Neogit pull<CR>', desc = '[G]it [p]ull' },
-    },
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {},
-    lazy = false,
-    keys = {
-      { '<leader>gbb', '<cmd>Gitsigns blame<CR>', desc = '[G]it [b]lame [b]uffer' },
-      { '<leader>gbl', '<cmd>Gitsigns blame_line<CR>', desc = '[G]it [b]lame [l]ine' },
-      { '<leader>ghp', '<cmd>Gitsigns preview_hunk_inline<CR>', desc = '[G]it [h]unk [p]review' },
-      { '<leader>ghl', '<cmd>Gitsigns setqflist<CR>', desc = '[G]it [h]unks [l]ist' },
-      { '<leader>gha', '<cmd>Gitsigns stage_hunk<CR>', desc = '[G]it [h]unks [a]dd' },
-      { '<leader>ghr', '<cmd>Gitsigns undo_stage_hunk<CR>', desc = '[G]it [h]unks [r]estore' },
     },
   },
   {
---
+++ nvim/dot-config/nvim/lua/plugins/git.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'sindrets/diffview.nvim',
+        lazy = false,
+        keys = {
+          { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = '[G]it diffview' },
+        },
+      },
+      'nvim-telescope/telescope.nvim',
+    },
+    opts = {},
+    keys = {
+      { '<leader>gg', '<cmd>Neogit kind=floating<CR>', desc = '[G]it' },
+      { '<leader>gp', '<cmd>Neogit pull<CR>', desc = '[G]it [p]ull' },
+    },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {},
+    lazy = false,
+    keys = {
+      { '<leader>gbb', '<cmd>Gitsigns blame<CR>', desc = '[G]it [b]lame [b]uffer' },
+      { '<leader>gbl', '<cmd>Gitsigns blame_line<CR>', desc = '[G]it [b]lame [l]ine' },
+      { '<leader>ghp', '<cmd>Gitsigns preview_hunk_inline<CR>', desc = '[G]it [h]unk [p]review' },
+      { '<leader>ghl', '<cmd>Gitsigns setqflist<CR>', desc = '[G]it [h]unks [l]ist' },
+      { '<leader>gha', '<cmd>Gitsigns stage_hunk<CR>', desc = '[G]it [h]unks [a]dd' },
+      { '<leader>ghr', '<cmd>Gitsigns undo_stage_hunk<CR>', desc = '[G]it [h]unks [r]estore' },
+    },
+  },
+}
