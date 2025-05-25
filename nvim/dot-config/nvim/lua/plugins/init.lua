df1ff1c546e7d650239cd32c9536fed83e09d036
grothej <justinmaxgrothe@gmail.com>
2025-05-25 21:30:16 +0200

    Move general plugins in plugins init.lua

---
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -0,0 +1,111 @@
+return {
+
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
+
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    opts = {
+      -- delay between pressing a key and opening which-key (milliseconds)
+      -- this setting is independent of vim.opt.timeoutlen
+      preset = 'helix',
+      delay = 300,
+      icons = {
+        -- set icon mappings to true if you have a Nerd Font
+        mappings = vim.g.have_nerd_font,
+      },
+
+      -- Document existing key chains
+      spec = {
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>p', group = '[P]rojects' },
+      },
+    },
+  },
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'yaml', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    keys = {
+      { '<leader>tt', ':NvimTreeFindFileToggle<CR>', desc = '[T]oggle Nvim[T]ree' },
+      { '<leader>tf', ':NvimTreeFocus<CR>', desc = '[T]oggle Nvim[T]ree [f]ocus ' },
+    },
+    opts = {
+      disable_netrw = true,
+      hijack_cursor = true,
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = {
+          enable = true,
+        },
+      },
+      modified = {
+        enable = true,
+      },
+      filters = {
+        custom = {},
+      },
+    },
+  },
+  {
+    'coffebar/neovim-project',
+    opts = {
+      projects = {
+        '~/projects/*',
+        '~/IdeaProjects/tc_code/*',
+        '~/IdeaProjects/tc_code/intranet/*',
+        '~/IdeaProjects/tc_code/corporate-website/*',
+        '~/dotfiles/*',
+        '~/programming/go-testing/go/',
+      },
+      picker = {
+        type = 'telescope',
+      },
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append 'globals' -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope.nvim' },
+      { 'Shatur/neovim-session-manager' },
+    },
+    lazy = false,
+    priority = 100,
+    keys = {
+      { '<leader>pp', ':NeovimProjectDiscover<CR>', desc = 'Search for [p]roject from specified patterns' },
+      { '<leader>pr', ':NeovimProjectHistory<CR>', desc = 'Search [p]roject from [r]ecent ones' },
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim', 'nvim-tree/nvim-web-devicons' }, -- if you use the mini.nvim suite
+    opts = {},
+  },
+}
---
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+return { -- Collection of various small independent plugins/modules
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.surround').setup()
+    require('mini.pairs').setup()
+    require('mini.move').setup()
+
+    local statusline = require 'mini.statusline'
+    -- set use_icons to true if you have a Nerd Font
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+  end,
+}
