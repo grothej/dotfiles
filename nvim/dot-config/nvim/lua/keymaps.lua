b65de0280a55d166a670dc2bc89b48de302d9bba
grothej <justinmaxgrothe@gmail.com>
2025-06-08 00:07:23 +0200

    Remove custom qf-list and loclist mappings

--- nvim/dot-config/nvim/lua/keymaps.lua
+++ nvim/dot-config/nvim/lua/keymaps.lua
@@ -1,40 +1,16 @@
 -- [[ Basic Keymaps ]]
 --
 
-vim.keymap.set('n', '<leader>tc', '<cmd>tabc<CR>')
+-- close tab, qf_list and loc_list
+vim.keymap.set('n', '<leader>tc', '<cmd>lclose<CR><cmd>cclose<CR><cmd>tabc<CR>')
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- copy whole file content
 vim.keymap.set('n', 'yw', '<cmd>%y<CR>', { desc = '[y]' })
 
--- sort selected lines
+--- sort selected lines
 vim.keymap.set('v', 'grs', '<cmd>sort<CR>', { desc = '[s]ort' })
-
--- qf list and loc list navigation mappings
---- execute the given command for a qf list or loc list of current window
---- @param cmd string
-function navigate_list(cmd)
-  local loc_list_length = vim.fn.getloclist(0, { size = 1 }).size
-  local qf_list_length = vim.fn.getqflist({ size = 1 }).size
-
-  if loc_list_length > 0 then
-    vim.cmd('l' .. cmd)
-  end
-
-  if qf_list_length > 0 then
-    vim.cmd('c' .. cmd)
-  end
-end
-vim.keymap.set('n', '<leader>c', function()
-  navigate_list 'close'
-end, { desc = 'close list' })
-vim.keymap.set('n', '<C-n>', function()
-  navigate_list 'next'
-end, { desc = 'next entry in list' })
-vim.keymap.set('n', '<C-p>', function()
-  navigate_list 'prev'
-end, { desc = 'next entry in list' })
 
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 -- Sync clipboard between OS and Neovim.
