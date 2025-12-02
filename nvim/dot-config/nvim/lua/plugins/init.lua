747807760f4df66ffe8067618e5fc17c7f4072b7
Justin Grothe <justin.grothe-ext@soundcloud.com>
2025-12-02 09:19:31 +0100

    feat: add nvim folding plugin

--- nvim/dot-config/nvim/lua/plugins/init.lua
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -104,4 +104,15 @@ return {
       { '<C-h>', [[<C-\><C-n><cmd>wincmd k<CR>]], mode = 't' },
     },
   },
+  {
+    'chrisgrieser/nvim-origami',
+    event = 'VeryLazy',
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
 }
