ee9a7013f5bd0f38c0e0037b70f068ba9af4510f
grothej <justinmaxgrothe@gmail.com>
2026-02-01 13:51:44 +0100

    chore: remove unused color-schemes

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,9 @@
 return {
   {
-    'catppuccin/nvim',
-    lazy = true,
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup {
-        flavour = 'auto', -- latte, frappe, macchiato, mocha
-        transparent_background = true, -- disables setting the background color.
-      }
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-  {
     'thesimonho/kanagawa-paper.nvim',
-    -- lazy = true,
     priority = 1000,
     init = function()
       vim.cmd 'colorscheme kanagawa-paper-ink'
-    end,
-    -- opts = {
-    --   transparent = true,
-    -- },
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require('kanagawa').load 'wave'
     end,
   },
 }
