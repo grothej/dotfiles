41f8565bb53a1897892628f15b99d2b813922048
grothej <justinmaxgrothe@gmail.com>
2026-02-11 22:59:30 +0100

    refactor: remove todo plugin

--- nvim/dot-config/nvim/lua/plugins/init.lua
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -30,9 +30,6 @@ return {
       },
     },
   },
-
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
